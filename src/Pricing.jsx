6a85fdf9801575d4a2169b7cf2e952f04365b27b
Monika Januszek <monika.januszek.mail@gmail.com>
2025-02-03 16:30:55 +0100

    back to project

--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -19,23 +19,22 @@ function Pricing() {
         <div className="option">
           <div className="option-title">Weekend trip</div>
           <div className="option-description">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod
-            commodi architecto ab eveniet incidunt placeat eum omnis illo
-            deleniti est recusandae a numquam, perferendis quaerat vitae,
-            consequatur quam odio, aliquam obcaecati nemo ad.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ullam
+            nobis fuga, libero voluptas repudiandae placeat neque dolore vel
+            eligendi pariatur facere assumenda optio! Minus laudantium
+            exercitationem natus aliquid ea, quasi perspiciatis alias, maiores
+            aliquam possimus sit, excepturi necessitatibus doloremque.
           </div>
           <div className="price">price to negotiate</div>
         </div>
         <div className="option">
           <div className="option-title">One week and longer</div>
           <div className="option-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            sint, delectus soluta aut odio distinctio, vitae eos voluptatibus
-            aspernatur nisi esse magni repellat aliquam fugiat officiis sit
-            eligendi molestiae in recusandae nobis obcaecati cupiditate non
-            quidem. Saepe veniam vitae et! Deserunt, consequuntur quidem. Minima
-            molestias nesciunt blanditiis debitis minus sed, harum quis! Odio,
-            omnis eligendi!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ullam
+            nobis fuga, libero voluptas repudiandae placeat neque dolore vel
+            eligendi pariatur facere assumenda optio! Minus laudantium
+            exercitationem natus aliquid ea, quasi perspiciatis alias, maiores
+            aliquam possimus sit, excepturi necessitatibus doloremque.
           </div>
           <div className="price">price to negotiate</div>
         </div>
