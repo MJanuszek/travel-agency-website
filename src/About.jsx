8f264ff429af460dc1434d1379bcf1fea74ed84c
Monika Januszek <monika.januszek.mail@gmail.com>
2025-03-10 12:56:31 +0100

    Credentials added

--- src/About.jsx
+++ src/About.jsx
@@ -7,11 +7,12 @@ function About() {
       <div className="info">
         <h2 className="about-title">Kim jestem?</h2>
         <p>
-          Na co dzień nauczycielem muzyki, weekenadmi przewodnikiem górkim.
+          Na co dzień nauczycielem muzyki, weekenadmi przewodnikiem górskim.
           Muzyka i góry – dwa światy, które wydają się odległe, a dla mnie
           stanowią jedność. Jako nauczyciel fortepianu uczę wrażliwości na
-          dźwięki i emocje. Jako przewodnik górski pokazuję piękno Sudetów i ich
-          natury. Odkryj, jak można znaleźć harmonię pomiędzy tymi pasjami.
+          dźwięki i emocje. Jako przewodnik pokazuję piękno Sudetów i górskiej
+          natury. Odkryj ze mną, jak można znaleźć harmonię pomiędzy tymi
+          pasjami.
           <br />
         </p>
         <div className="signature">Piotr</div>
--- src/Credentials.jsx
+++ src/Credentials.jsx
@@ -6,8 +6,32 @@ function Credentials() {
       <div className="credentials" id="credentials">
         <div className="storyteller">
           <h1 className="credentials-title">
-            Każda wędrówka ma swój rytm, melodia swoją przestrzeń
+            Wędrówka ma swój rytm, melodia swoją przestrzeń
           </h1>
+          <div>
+            <p className="story">
+              W górach każdy krok wyznacza tempo marszu – czasem jest to
+              powolne, miarowe stawianie stóp na miękkim podłożu leśnej ścieżki,
+              a czasem dynamiczne wspinanie się po skalistym zboczu, gdzie
+              oddech staje się krótszy, a serce przyspiesza. Tak jak w muzyce,
+              gdzie frazy układają się w większą całość, tak i wędrówka składa
+              się z etapów: spokojnego wejścia w rytm, intensywnego podejścia,
+              kulminacji na szczycie i powolnego wyciszenia podczas zejścia.
+            </p>
+            <p className="story">
+              Wędrując, czujemy, jak przestrzeń wokół nas zmienia swój charakter
+              – z gęstego lasu wychodzimy na otwarte polany, gdzie horyzont
+              poszerza się niczym dźwięk rozchodzący się w katedrze. Wiatr
+              szumiący w koronach drzew to muzyczne tło dla naszych kroków, a
+              śpiew ptaków i plusk strumienia uzupełniają symfonię natury.
+            </p>
+            <p className="story">
+              W górach, podobnie jak w muzyce, liczy się uważność – na
+              otaczające nas dźwięki, rytm naszego ciała, oddech i krajobraz.
+              Górska przestrzeń, niczym muzyczna fraza, wypełnia ciszę i daje
+              możliwość odnalezienia wewnętrznej harmonii.
+            </p>
+          </div>
         </div>
         <h1 className="credentials-title">Poznaj mnie lepiej</h1>
         <div className="insta">
