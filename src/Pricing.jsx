dd2c631eea39ba8fd44130409f5e2d55e40534e6
Monika Januszek <monika.januszek.mail@gmail.com>
2025-03-11 13:28:56 +0100

    pricing section

--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -1,43 +1,76 @@
 import "./style/Pricing.scss";
 import "./style/Text.scss";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 function Pricing() {
   return (
     <div className="pricing-section" id="pricing">
-      <h1 className="section-title">Oferta</h1>
+      <h1 className="title">Oferta</h1>
       <div className="price-options">
         <div className="option">
-          <div className="option-title">One day trip</div>
-          <div className="option-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ullam
-            nobis fuga, libero voluptas repudiandae placeat neque dolore vel
-            eligendi pariatur facere assumenda optio! Minus laudantium
-            exercitationem natus aliquid ea, quasi perspiciatis alias, maiores
-            aliquam possimus sit, excepturi necessitatibus doloremque.
+          <div className="title">Wycieczka dzienna</div>
+          <div className="story">
+            Masz tylko jeden dzień, ale chcesz poczuć magię gór? Zapraszam na
+            wycieczkę pełną niezapomnianych widoków, lokalnych historii i
+            przyrody Sudetów. Wybierzemy trasę dostosowaną do Twoich możliwości
+            – od łagodnych spacerów po bardziej wymagające szlaki. Idealna opcja
+            na szybki reset w naturze!
           </div>
-          <div className="price">price to negotiate</div>
+
+          <Link
+            className="menu-link"
+            activeClass="active"
+            to="booking"
+            spy={true}
+            smooth={true}
+            duration={500}
+            offset={-70}
+          >
+            Zarezerwuj wycieczkę
+          </Link>
         </div>
         <div className="option">
-          <div className="option-title">Weekend trip</div>
-          <div className="option-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ullam
-            nobis fuga, libero voluptas repudiandae placeat neque dolore vel
-            eligendi pariatur facere assumenda optio! Minus laudantium
-            exercitationem natus aliquid ea, quasi perspiciatis alias, maiores
-            aliquam possimus sit, excepturi necessitatibus doloremque.
+          <div className="title">Weekend </div>
+          <div className="story">
+            Chcesz uciec od codzienności i spędzić dwa dni w sercu Sudetów?
+            Proponuję weekendową wyprawę, podczas której nie tylko przejdziemy
+            malownicze szlaki, ale także poznamy lokalne legendy i ciekawostki.
+            To doskonała okazja, by naładować energię, doświadczyć piękna gór i
+            poczuć ich wyjątkowy klimat!
           </div>
-          <div className="price">price to negotiate</div>
+          <Link
+            className="menu-link"
+            activeClass="active"
+            to="booking"
+            spy={true}
+            smooth={true}
+            duration={500}
+            offset={-70}
+          >
+            Zarezerwuj wycieczkę
+          </Link>
         </div>
         <div className="option">
-          <div className="option-title">One week and longer</div>
-          <div className="option-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ullam
-            nobis fuga, libero voluptas repudiandae placeat neque dolore vel
-            eligendi pariatur facere assumenda optio! Minus laudantium
-            exercitationem natus aliquid ea, quasi perspiciatis alias, maiores
-            aliquam possimus sit, excepturi necessitatibus doloremque.
+          <div className="title">Dłuższa przygda</div>
+          <div className="story">
+            Pragniesz dłuższej przygody? Wybierz kilkudniowy trekking po
+            Sudetach. Wędrówka przez różne krajobrazy – od tajemniczych lasów po
+            skalne grzbiety – pozwoli poczuć magię tych gór. Każdy dzień to nowe
+            odkrycia, historie zapisane w krajobrazie i chwile, które pozwalają
+            oderwać się od codzienności. Głębokie zanurzenie się w naturze i
+            znalezienie własnej harmonii wśród górskich ścieżek.
           </div>
-          <div className="price">price to negotiate</div>
+          <Link
+            className="menu-link"
+            activeClass="active"
+            to="booking"
+            spy={true}
+            smooth={true}
+            duration={500}
+            offset={-70}
+          >
+            Zarezerwuj wycieczkę
+          </Link>
         </div>
       </div>
     </div>
